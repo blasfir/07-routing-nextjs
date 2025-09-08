51e7a4e2cfed66fa9c654c5369f0120445d4e28e
Kate <Katezador-ipt27@lll.kpi.ua>
2025-09-08 23:41:24 +0300

    add edits

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -18,6 +18,7 @@ export default function NotePreview({ id }: NotePreviewProps) {
     queryKey: ["note", id],
     queryFn: () => fetchNoteById(id),
     enabled: !!id,
+    refetchOnMount: false,
   });
 
   if (isLoading) {
